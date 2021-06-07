a8437b948a0db0815f5353f2df8b592e8551dce6
Mel Jack <melissa_jack@hotmail.co.uk>
2021-06-07 18:38:34 +0100

    Add calculator functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,9 +14,9 @@ function divide(a, b) {
     return a / b; 
 }
 
-function operate(operator, no1, no2) {
-    let a = Number(no1);
-    let b = Number(no2);
+function operate(no1, no2, operator) {
+    const a = Number(no1);
+    const b = Number(no2);
     switch(operator) {
         case "add": 
             return add(a, b);
@@ -33,77 +33,122 @@ function operate(operator, no1, no2) {
     }
 }
 
-const screen = document.querySelector('#screen')
+const calculator = {
+    no1: "",
+    no2: "",
+    operator: null,
+    isSum: false
+}; 
 
+function addNumber(number) {
+    if (calculator.isSum) {
+        document.getElementById("display").textContent = "";
+        calculator.isSum = false;
+    }
+    if (calculator.operator) {
+        calculator.no2 += number;
+    } else {
+        calculator.no1 += number;
+    }
+}
 
 const one = document.querySelector('#one');
 one.addEventListener('click', () => {
+    addNumber("1");
     document.getElementById("display").textContent += "1";
 });
 
 const two = document.querySelector('#two');
 two.addEventListener('click', () => {
+    addNumber("2");
     document.getElementById("display").textContent += "2";
 });
 
 const three = document.querySelector('#three');
 three.addEventListener('click', () => {
+    addNumber("3");
     document.getElementById("display").textContent += "3";
 });
 
 const four = document.querySelector('#four');
 four.addEventListener('click', () => {
+    addNumber("4");
     document.getElementById("display").textContent += "4";
 });
 
 const five = document.querySelector('#five');
 five.addEventListener('click', () => {
+    addNumber("5");
     document.getElementById("display").textContent += "5";
 });
 
 const six = document.querySelector('#six');
 six.addEventListener('click', () => {
+    addNumber("6");
     document.getElementById("display").textContent += "6";
 });
 
 const seven = document.querySelector('#seven');
 seven.addEventListener('click', () => {
+    addNumber("7");
     document.getElementById("display").textContent += "7";
 });
 
 const eight = document.querySelector('#eight');
 eight.addEventListener('click', () => {
+    addNumber("8");
     document.getElementById("display").textContent += "8";
 });
 
 const nine = document.querySelector('#nine');
 nine.addEventListener('click', () => {
+    addNumber("9");
     document.getElementById("display").textContent += "9";
 });
 
 const zero = document.querySelector('#zero');
 zero.addEventListener('click', () => {
+    addNumber("0");
     document.getElementById("display").textContent += "0";
 });
 
 const addButton = document.querySelector('#add');
 addButton.addEventListener('click', () => {
+    calculator.operator = "add";
     document.getElementById("display").textContent += "+";
 });
 
 const multiplyButton = document.querySelector('#multiply');
 multiplyButton.addEventListener('click', () => {
+    calculator.operator = "multiply";
     document.getElementById("display").textContent += "×";
 });
 
 const divideButton = document.querySelector('#divide');
 divideButton.addEventListener('click', () => {
+    calculator.operator = "divide";
     document.getElementById("display").textContent += "÷";
 });
 
 const minusButton = document.querySelector('#minus');
 minusButton.addEventListener('click', () => {
+    calculator.operator = "subtract";
     document.getElementById("display").textContent += "-";
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    document.getElementById("display").textContent = "";
+});
+ 
+const equalsButton = document.querySelector('#equals'); 
+equalsButton.addEventListener('click', () => {
+    const sum = operate(calculator.no1, calculator.no2, calculator.operator);
+    document.getElementById("display").textContent = sum;
+    calculator.no1 = "";
+    calculator.no2 = "";
+    calculator.operator = null;
+    calculator.isSum = true;
 });
 
  
